eafff1f978e4a34f9b424027645887a8486de752
Andrew Sears <searsandrew@gmail.com>
2019-07-15 11:47:45 -0500

    Plugin Init

---
+++ familyarchive.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Family Archive
+Plugin URI: http://www.mayfifteenth.com/
+Description: Family Archive is a plugin that provides an easy way to upload and tag photos for family members to see and still use the fiull features of WordPress where desired.
+Version: 1.0
+Author: Andrew Sears
+Author URI: http://www.mayfifteenth.com
+*/
