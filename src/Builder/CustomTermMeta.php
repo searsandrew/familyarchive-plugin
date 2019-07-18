2a552b8969527e3c0e5dad1a5970ff1abe2e819f
Andrew Sears <searsandrew@gmail.com>
2019-07-18 14:09:30 -0500

    Update CustomTermMeta.php

--- src/Builder/CustomTermMeta.php
+++ src/Builder/CustomTermMeta.php
@@ -37,10 +37,10 @@ class CustomTermMeta
     {
             add_action('admin_enqueue_scripts', array( $this, 'mediaLibUploaderEnqueue'));
 			add_action( $this->term . '_add_form_fields',  array( $this, 'createScreenFields'), 10, 1 );
-			// add_action( $this->term . '_edit_form_fields', array( $this, 'editScreenFields' ),  10, 2 );
+			add_action( $this->term . '_edit_form_fields', array( $this, 'editScreenFields' ),  10, 2 );
 
-			// add_action( 'created_' . $this->term, array( $this, 'save_data' ), 10, 1 );
-			// add_action( 'edited_' . $this->term,  array( $this, 'save_data' ), 10, 1 );
+			add_action( 'created_' . $this->term, array( $this, 'save_data' ), 10, 1 );
+			add_action( 'edited_' . $this->term,  array( $this, 'save_data' ), 10, 1 );
     }
 
     /**
@@ -48,24 +48,50 @@ class CustomTermMeta
      */
     public function createScreenFields($taxonomy)
     {
-        $variable = sprintf('cf_%s_image', $taxonomy);
+		$imageValue = '';
 
-		$$variable = '';
-
-        printf('<div class="form-field term-m15_%1$s_image-wrap">
-            <label for="m15_%1$s_image">Image</label>
-            <input id="image-url" type="text" name="cf_%1$s_image" value="' . esc_attr( $$variable ) . '" />
+        printf('<div class="form-field term-cf_%1$s_image-wrap">
+            <label for="cf_%1$s_image">Image</label>
+            <input id="image-url" type="text" name="cf_%1$s_image" value="%2$s" />
             <input id="upload-button" type="button" class="button" value="Upload Image" />
-        </div>', $taxonomy);
+        </div>', $taxonomy, esc_attr($imageValue));
+    }
 
-		// // Form fields.
-		// echo '';
-		// echo '	';
-		// echo '	<input type="number" id="cf_place_image" name="cf_place_image" placeholder="' . 'Image ID' . '" value="' . esc_attr( $cf_place_image ) . '">';
-		// echo '	<p class="description">' . 'Paste link to image you\'d like to use.' . '</p>';
-		// echo '</div>';
+    /**
+     * Edit screen fields builds the form fields for the edit screen
+     */
+    public function editScreenFields( $term, $taxonomy )
+    {
+		$imageValue = get_term_meta( $term->term_id, 'cf_'.$taxonomy.'_image', true );
+
+        if( empty( $imageValue ) ) $imageValue = '';
+        
+        printf('<tr class="form-field term-cf_%1$s_image-wrap">
+            <th scope="row">
+                <label for="cf_%1$s_image">Image</label>
+            </th>
+            <td>
+                <input id="image-url" type="text" name="cf_%1$s_image" value="%2$s" />
+                <input id="upload-button" type="button" class="button" value="Upload Image" />
+            </td>
+        </tr>', $taxonomy, esc_attr( $imageValue ));
     }
     
+    /**
+     * Save the data from the media uploader
+     */
+    public function save_data( $term_id )
+    {
+        $saveValue = sprintf('cf_%s_image', $this->term);
+
+		$cf_new_image = isset( $_POST[ $saveValue ] ) ? $_POST[ $saveValue ] : '';
+
+		update_term_meta( $term_id, $saveValue, $cf_new_image );
+	}
+
+    /**
+     * Enqueue the scripts needed to make the image uplader work correctly
+     */
     function mediaLibUploaderEnqueue() {
         wp_enqueue_media();
         wp_register_script( 'media-lib-uploader-js', plugins_url( '../../assets/js/media-lib-uploader.js' , __FILE__ ), array('jquery') );
